db160c3ad01d13825362e8b14692220b82ece844
Lea9723 <leastattner@gmail.com>
2024-03-08 16:25:19 +0100

    creation branche React bouton test

---
+++ dom-react/components/test-input.jsx
@@ -0,0 +1,14 @@
+function Input () {
+
+return (
+    <>
+    <label><input placeholder="entre ton nom" type="text"/></label>
+    <button>entrer</button>
+
+    </>
+)
+}
+
+
+
+export default Input
---
+++ dom-react/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Input from '../components/test-input'
+
+function App() {
+
+  return (
+  <>
+<Input />
+<Input />
+
+     
+    </>
+  )
+}
+
+export default App
+
+
